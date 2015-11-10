7d531d582107d198ae317139210384fe227ece77
Jan Kučera <jan.kucera@envelon.cz>
2015-11-10 16:33:47 +0100

    Add grails and gradle file path to the $PATH variable

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 set -g -x fish_greeting ''
-
+set -U JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
 set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
 set PATH $PATH /usr/local/mysql/bin/
 set PATH $PATH ~/.jenv/bin
+set PATH $PATH ~/.sdkman/candidates/grails/current/bin
+set PATH $PATH ~/.sdkman/candidates/gradle/current/bin
