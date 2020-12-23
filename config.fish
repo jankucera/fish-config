dc8ab46474d10229096ece794122b7bc79fe7c90
Jan Kučera <jan@Jans-MacBook-Air.local>
2020-12-23 10:17:27 +0100

    Modified config.fish to include mysql@5.6 paths

--- config.fish
+++ config.fish
@@ -1,7 +1,5 @@
 set -g -x fish_greeting ''
-set -U JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
-set PATH $PATH (find ~/.gvm/*/current/bin -maxdepth 0)
+
 set PATH $PATH /usr/local/mysql/bin/
-set PATH $PATH ~/.jenv/bin
-set PATH $PATH ~/.sdkman/candidates/grails/current/bin
-set PATH $PATH ~/.sdkman/candidates/gradle/current/bin
+set PATH $PATH /usr/local/sbin
+set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
